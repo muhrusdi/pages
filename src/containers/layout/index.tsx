373e1bacf51fc3ba2337f2a1097422b3eb0efb69
mhmmr <mhmmrco@gmail.com>
2020-12-06 09:57:21 +0000

    fix

--- src/containers/layout/index.tsx
+++ src/containers/layout/index.tsx
@@ -3,11 +3,15 @@ import SEO from "components/seo"
 import { LayoutType } from "types"
 
 const Layout: React.FC<LayoutType> = ({children}) => {
+  const date = new Date()
   return (
     <>
       <SEO/>
-      <div className="container">
+      <div className="container max-w-screen-lg">
         {children}
+        <div className="text-center py-16">
+          <span>© {date.getFullYear()} Made with <span className="text-red-500">♥</span> - mhmmr</span>
+        </div>
       </div>
     </>
   )
--- src/pages/index.ts
+++
@@ -1,3 +0,0 @@
-import Home from "templates/home"
-
-export default Home
