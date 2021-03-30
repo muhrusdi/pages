cc44a6fa987de7b5ff28ce6b8bf8fbd6f48bbfae
Muhammad Rusdi <muhammadrusdiid@gmail.com>
2021-03-30 14:32:31 +0000

    fix build

--- src/components/utils/index.jsx
+++ src/components/utils/index.jsx
@@ -46,6 +46,17 @@ export const HomeHeaderStyled = styled.div`
   }
 `;
 
+export const BlogItemFooter = styled.div`
+  a {
+    &:hover {
+      color: rgba(156, 163, 175, var(--tw-text-opacity));
+      & + span {
+        color: white;
+      }
+    }
+  }
+`
+
 export const mocks = [
   {
     title: "Profile",
