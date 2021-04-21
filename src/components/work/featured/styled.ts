1095b0f6e3a7af2c35170a720babb8d3dd429c70
Muhammad Rusdi <muhammadrusdiid@gmail.com>
2021-04-21 21:47:41 +0000

    featured work

---
+++ src/components/work/featured/styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const ImgBg = styled.div`
+  background: url("../../../images/senja-boilerplate-cover.png") center;
+  background-size: contain;
+  height: 400px;
+`
