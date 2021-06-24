02760ee3cbbbe89df3a59b262babf1a82c5ffcec
Muhammad Rusdi <muhammadrusdiid@gmail.com>
2021-06-24 15:44:35 +0000

    update menu icon

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -43,16 +43,36 @@ const Nav: React.FC = () => {
             <CenterMenus ref={menusRef}>
               <ul className={`sm:flex justify-center -mx-2 uppercase text-sm border border-gray-700 sm:border-none rounded-lg bg-black items-center ${toggle ? "" : "hidden"}`}>
                 <li className="px-2">
-                  <Link to="/blog" className="text-gray-200 hover:text-white transition-all">Blog</Link>
+                  <div className="flex items-center">
+                    <div className="mr-3 sm:hidden rounded-md bg-gradient-to-br from-green-400 to-green-600">
+                      <svg className="h-8 w-8" viewBox="0 0 24 24"><path d="M8 9a1 1 0 011-1h8a1 1 0 011 1v7.5a1.5 1.5 0 01-1.5 1.5h-7A1.5 1.5 0 018 16.5V9z" fill="#6EE7B7"></path><path d="M15 7a1 1 0 00-1-1H7a1 1 0 00-1 1v9.5A1.5 1.5 0 007.5 18H16v-.085a1.5 1.5 0 01-1-1.415V7z" fill="#ECFDF5"></path><path fill="#A7F3D0" d="M8 8h5v4H8zM8 14h5v2H8z"></path></svg>
+                    </div>
+                    <Link to="/blog" className="text-gray-200 hover:text-white transition-all">Blog</Link>
+                  </div>
                 </li>
                 <li className="px-2">
-                  <Link to="/work" className="text-gray-200 hover:text-white transition-all">Work</Link>
+                  <div className="flex items-center">
+                    <div className="mr-3 rounded-md bg-gradient-to-br from-yellow-400 to-yellow-600">
+                      <svg className="h-8 w-8" viewBox="0 0 24 24"><path fillRule="evenodd" clipRule="evenodd" d="M13.196 6.02a1 1 0 01.785 1.176l-2 10a1 1 0 01-1.961-.392l2-10a1 1 0 011.176-.784z" fill="#FDE68A"></path><path fillRule="evenodd" clipRule="evenodd" d="M15.293 9.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-2 2a1 1 0 01-1.414-1.414L16.586 12l-1.293-1.293a1 1 0 010-1.414zM8.707 9.293a1 1 0 010 1.414L7.414 12l1.293 1.293a1 1 0 11-1.414 1.414l-2-2a1 1 0 010-1.414l2-2a1 1 0 011.414 0z" fill="#FDF4FF"></path></svg>
+                    </div>
+                    <Link to="/work" className="text-gray-200 hover:text-white transition-all">Work</Link>
+                  </div>
                 </li>
                 <li className="px-2">
-                  <a href="#" className="text-gray-200 hover:text-white transition-all">Story</a>
+                  <div className="flex items-center">
+                    <div className="mr-3 rounded-md bg-gradient-to-br from-pink-400 to-purple-600">
+                      <svg className="h-8 w-8" viewBox="0 0 24 24"><circle cx="12" cy="12" r="7" fill="#F3E8FF"></circle><path d="M14.52 11.136a1 1 0 010 1.728l-3.016 1.759A1 1 0 0110 13.759v-3.518a1 1 0 011.504-.864l3.015 1.76z" fill="#C084FC"></path></svg>
+                    </div>
+                    <a href="#" className="text-gray-200 hover:text-white transition-all">Story</a>
+                  </div>
                 </li>
                 <li className="px-2">
-                  <Link to="/profile" className="text-gray-200 hover:text-white transition-all">Profile</Link>
+                  <div className="flex items-center">
+                    <div className="mr-3 rounded-md bg-gradient-to-br from-purple-500 to-indigo-500">
+                      <svg className="h-8 w-8" viewBox="0 0 24 24"><path d="M6 9l6-3 6 3v6l-6 3-6-3V9z" fill="#F5F3FF"></path><path d="M6 9l6 3v6l-6-3V9z" fill="#DDD6FE"></path><path d="M18 9l-6 3v6l6-3V9z" fill="#C4B5FD"></path></svg>
+                    </div>
+                    <Link to="/profile" className="text-gray-200 hover:text-white transition-all">Profile</Link>
+                  </div>
                 </li>
                 <GithubItem className="sm:px-2 sm:ml-2 py-2 px-1 rounded-md mt-2 sm:mt-0 flex justify-center bg-gray-900 sm:bg-transparent sm:p-0 sm:block">
                   <a href="https://github.com/muhrusdi" target="_blank" rel="noreferrer" className="block">
