70ca2a8740224d4acee1aeac7e5c5c98ca95edae
muhrusdi <mhmrofficial@gmail.com>
2021-06-28 10:31:58 +0800

    remove layout

--- src/templates/work/index.tsx
+++ src/templates/work/index.tsx
@@ -46,55 +46,53 @@ const Works: React.FC = () => {
   const { loading, data: dataWorks } = useQuery(WORK_COLLECTION)
 
   return (
-    <>
-        <div>
-          <div className="mt-8">
-            <FeaturedWork/>
-          </div>
-          <div className="mt-32">
-            <ul className="grid grid-cols-1 mt-8 sm:grid-cols-2 md:grid-cols-3 gap-8">
-              {
-                loading ? (
-                  <>
-                    <li>
-                      <WorkItemLoading/>
-                    </li>
-                    <li>
-                      <WorkItemLoading/>
-                    </li>
-                    <li>
-                      <WorkItemLoading/>
-                    </li>
-                  </>
-                ) : null
-              }
-              {
-                dataWorks ? (
-                  dataWorks.workCollection.items.map((data, i) => (
-                    <li key={i}>
-                      <WorkItem data={data}/>
-                    </li>
-                  ))
-                ) : null
-              }
-            </ul>
-          </div>
-          <div className="mt-32">
-            <div className="text-center">
-              <h2 className="text-2xl font-bold">Open Source Contribution</h2>
-            </div>
-            <ul className="grid grid-cols-1 mt-8 sm:grid-cols-3 md:grid-cols-4 gap-8">
-              {
-                dataWorks?.openSourceCollection.items.map((item, i) => (
-                  <li key={i}>
-                    <WorkItem.Mini data={item}/>
-                  </li>
-                ))
-              }
-            </ul>
-          </div>
+    <div>
+      <div className="mt-8">
+        <FeaturedWork/>
+      </div>
+      <div className="mt-32">
+        <ul className="grid grid-cols-1 mt-8 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {
+            loading ? (
+              <>
+                <li>
+                  <WorkItemLoading/>
+                </li>
+                <li>
+                  <WorkItemLoading/>
+                </li>
+                <li>
+                  <WorkItemLoading/>
+                </li>
+              </>
+            ) : null
+          }
+          {
+            dataWorks ? (
+              dataWorks.workCollection.items.map((data, i) => (
+                <li key={i}>
+                  <WorkItem data={data}/>
+                </li>
+              ))
+            ) : null
+          }
+        </ul>
+      </div>
+      <div className="mt-32">
+        <div className="text-center">
+          <h2 className="text-2xl font-bold">Open Source Contribution</h2>
         </div>
-    </>
+        <ul className="grid grid-cols-1 mt-8 sm:grid-cols-3 md:grid-cols-4 gap-8">
+          {
+            dataWorks?.openSourceCollection.items.map((item, i) => (
+              <li key={i}>
+                <WorkItem.Mini data={item}/>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
   )
 }
 
