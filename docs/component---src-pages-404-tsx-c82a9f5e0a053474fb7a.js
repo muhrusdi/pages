9f4fd39339aad6cd86a784ad538f5dd306cb2b7f
muhrusdi <muhrusdi@users.noreply.github.com>
2021-06-28 00:59:36 +0000

    Automated Change

--- docs/component---src-pages-404-tsx-c82a9f5e0a053474fb7a.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkmuhrusdi_github_io=self.webpackChunkmuhrusdi_github_io||[]).push([[218],{962:function(e,t,u){"use strict";u.r(t),u.d(t,{default:function(){return i}});var n=u(7294),l=u(5391),r=u(3411),i=function(){return n.createElement(l.Z,null,n.createElement(r.Z,{title:"404: Not found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-c82a9f5e0a053474fb7a.js.map
--- docs/sw.js
+++ docs/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3df4c39f29cf4e9d3798.js"
+    "url": "webpack-runtime-fcce9550bb6738bb222d.js"
   },
   {
     "url": "framework-2bd93b233355e255d736.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "styles.0da3d732db7ca9d470ed.css"
   },
   {
-    "url": "app-721c3ce613b69342bb64.js"
+    "url": "app-7900bd8b7b76f912e2e8.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d50c857b5222c6f3337934b1f14d7444"
+    "revision": "58d424fa6755f90b719b771c131a3f43"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-56ae04524340337563b8.js"
@@ -142,7 +142,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-721c3ce613b69342bb64.js`))) {
+  if (!resources || !(await caches.match(`/app-7900bd8b7b76f912e2e8.js`))) {
     return await fetch(event.request)
   }
 
