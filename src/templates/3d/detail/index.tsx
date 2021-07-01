49058deb633b43f50310e41a4b0bb10ce0bee72f
muhrusdi <mhmrofficial@gmail.com>
2021-07-01 19:00:23 +0800

    update eslint

--- src/templates/3d/detail/index.tsx
+++ src/templates/3d/detail/index.tsx
@@ -1,19 +1,27 @@
 import React, { Suspense } from "react"
 import { Canvas, useLoader } from "react-three-fiber"
 // import * as THREE from "three"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
-import { OrbitControls, Stage, Environment } from "@react-three/drei"
+import { OrbitControls, Stage, Environment, useGLTF } from "@react-three/drei"
 
 const Model = ({ url }) => {
-  const { nodes, materials } = useLoader(GLTFLoader, url)
-  console.log(nodes, materials)
+  const {nodes, materials} = useGLTF(url)
   return (
-    <group rotation={[Math.PI / 1, 2, 3]} position={[20, 20, 20]} scale={[2, 2, 2]}>
-      <group rotation={[Math.PI / 13.5, -Math.PI / 5, Math.PI / 5]}>
+    <group position={[0, -3, 0]} scale={[4, 4, 4]}>
+      <group rotation={[Math.PI / 10.5, -Math.PI / 3.8, Math.PI / 7.6]}>
+        {/* <mesh castShadow receiveShadow geometry={nodes.planet001.geometry} material={materials.scene} />
+        <mesh castShadow receiveShadow geometry={nodes.planet002.geometry} material={materials.scene} /> */}
         <mesh castShadow receiveShadow geometry={nodes.Cube_1.geometry} material={materials.Base} />
         <mesh castShadow receiveShadow geometry={nodes.Cube_2.geometry} material={materials.Rubber} />
         <mesh castShadow receiveShadow geometry={nodes.Cube_3.geometry} material={materials.Material} />
-        <mesh castShadow receiveShadow geometry={nodes.Cube_4.geometry} material={materials.Inner} />
+        <mesh castShadow receiveShadow geometry={nodes.Cube_4.geometry} material={materials.Rubber} />
+      </group>
+      <group position={[3, 0.4, 2]} rotation={[Math.PI / 10.5, -Math.PI / 3.8, Math.PI / 7.6]}>
+        {/* <mesh castShadow receiveShadow geometry={nodes.planet001.geometry} material={materials.scene} />
+        <mesh castShadow receiveShadow geometry={nodes.planet002.geometry} material={materials.scene} /> */}
+        <mesh castShadow receiveShadow geometry={nodes.Cube_1.geometry} material={materials.Base} />
+        <mesh castShadow receiveShadow geometry={nodes.Cube_2.geometry} material={materials.Rubber} />
+        <mesh castShadow receiveShadow geometry={nodes.Cube_3.geometry} material={materials.Material} />
+        <mesh castShadow receiveShadow geometry={nodes.Cube_4.geometry} material={materials.Rubber} />
       </group>
     </group>
   )
@@ -25,16 +33,13 @@ const DetailThreeD: React.FC = () => {
     <div>
       <div className="grid gap-8 grid-cols-2">
         <div>
-          <div className="bg-blueGray-900 rounded-md">
-            <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 7.5] }} style={{height: 400}}>
-            <pointLight color="indianred" />
-    <pointLight position={[10, 10, -10]} color="orange" />
-    <pointLight position={[-10, -10, 10]} color="lightblue" />
+          <div className="bg-blueGray-900 rounded-md" style={{height: 400}}>
+            <Canvas shadowMap camera={{ position: [0, 0, 15] }} className="rounded-md">
               <Suspense fallback={null}>
-              <Environment path="/cube" />
-                <Stage environment={null} intensity={0.3} contactShadowOpacity={0.3} shadowBias={-0.0015}>
+                <Environment path="/cube" />
+                {/* <Stage environment={null} intensity={0.3} contactShadowOpacity={0.3} shadowBias={-0.0015}> */}
                   <Model url="/shoes.gltf" />
-                </Stage>
+                {/* </Stage> */}
               </Suspense>
               <OrbitControls
                 autoRotate
@@ -50,7 +55,15 @@ const DetailThreeD: React.FC = () => {
           </div>
         </div>
         <div>
-          right
+          <div className="h-full flex flex-col">
+            <div style={{flexBasis: "100%"}}>
+              <h2 className="text-4xl font-bold">Shoes</h2>
+              <p className="text-gray-300 mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, dignissimos!</p>
+            </div>
+            <div>
+              <button className="w-full bg-indigo-600 font-semibold text-white p-3 rounded-md">Download</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/templates/3d/index.tsx
+++ src/templates/3d/index.tsx
@@ -1,9 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const ThreeDPage: React.FC = () => {
   return (
     <div>
-      3d
+      <div className="grid grid-cols-4 gap-8">
+        <div>
+          <Link to="/3d/shoes" className="block">
+            <img className="object-cover rounded-lg h-44 w-full" src="https://res.cloudinary.com/muhrusdi/image/upload/v1625136846/sepatuh.jpg" alt="" />
+            <div className="mt-2">
+              <h3 className="font-semibold text-lg">Shoes</h3>
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
