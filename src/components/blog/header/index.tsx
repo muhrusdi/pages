01b06286e64a44c5d00a0f7f30275fb851352cd3
muhrusdi <mhmrofficial@gmail.com>
2021-12-19 17:56:49 +0800

    update banner

--- src/components/blog/header/index.tsx
+++ src/components/blog/header/index.tsx
@@ -5,7 +5,7 @@ import { BiChevronRight } from "react-icons/bi"
 const SectionHeader: React.FC = ({ tag, title, description, link }) => (
   <div className="mb-4 sm:w-1/2">
     <h2 className="uppercase font-bold text-indigo-500">{tag}</h2>
-    <h3 className="font-black mt-3 text-4xl">{title}</h3>
+    <h3 className="font-extrabold mt-3 text-4xl">{title}</h3>
     <p className="text-lg mt-4 text-gray-400">{description}</p>
     <Link to={link} className="mt-8 inline-block text-lg">
       <div className="flex items-center">
--- src/components/playcode/index.tsx
+++ src/components/playcode/index.tsx
@@ -1,33 +1,19 @@
 import React, { useRef, useState, memo, useCallback } from "react"
 import Editor, { loader } from "@monaco-editor/react"
+import { Link } from "gatsby"
 import { parseToReact } from "@/components/utils"
 import nightOwl from "monaco-themes/themes/Night Owl.json"
 import { EditorStyled } from "./styled"
+import { Tooltip, TooltipTrigger, StyledContent, StyledArrow } from "../tooltip"
 
-const snippet = `<div>
-  <div className="bg-blueGray-700 rounded-full inline-flex items-center text-xs uppercase pr-4">
-    <span className="bg-indigo-600 rounded-l-full px-3 text-xs font-medium py-1 mr-2">
-      Package
-    </span>
-    Play
-  </div>
-  <h2 className="text-4xl sm:text-5xl sm:leading-12 mt-6 font-black line-clamp-3">
-    Shipless Play
-  </h2>
-  <p className="mt-4 sm:mt-8 text-lg text-gray-400 line-clamp-2">
-    Simple playground for HTML, CSS and JavaScript
-  </p>
-  <a href="https://github.com/shipless/play" target="_blank" rel="noreferrer" className="mt-8 sm:mt-10 inline-block text-lg">
-    <div className="flex items-center">
-      <span>
-        Get Package
-      </span>
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-      </svg>
-    </div>
-  </a>
-</div>
+const snippet = `<h2 className="text-4xl sm:text-4xl sm:leading-12 mt-6 font-black line-clamp-3">
+  Developed
+  <span className="text-pink-500">high-quality</span> 
+  user
+  interface using
+  <span className="text-blue-light">React</span>{" "}
+  ecosystem.
+</h2>
 `
 
 const MemoEditor = memo(({ defaultValue, onMount, onChange }) => (
@@ -70,7 +56,54 @@ const PlayCode: React.FC = () => {
   return (
     <div className="md:flex -mx-6 items-center">
       <div className="w-full md:w-1/2 px-6">
-        {parseToReact(value || snippet)}
+        <div>
+          <div>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <img
+                  className="h-12 w-12 object-cover cursor-pointer rounded-full ring-4 ring-purple-600"
+                  src="https://res.cloudinary.com/muhrusdi/image/upload/v1616335341/thumb-612467.png"
+                  alt=""
+                />
+              </TooltipTrigger>
+              <StyledContent side="right" sideOffset={5}>
+                <span className="font-bold">I&apos;m Rusdi</span>
+                <StyledArrow />
+              </StyledContent>
+            </Tooltip>
+          </div>
+          {parseToReact(value || snippet)}
+          {/* <h2 className="text-4xl sm:text-4xl sm:leading-12 mt-6 font-black line-clamp-3">
+            Developed <span className="text-pink-500">high-quality</span> user
+            interface using <span className="text-blue-light">React</span>{" "}
+            ecosystem.
+          </h2> */}
+          {/* <p className="mt-4 sm:mt-8 text-lg text-gray-400 line-clamp-2">
+            Develop high quality User Interface using React.
+          </p> */}
+          <Link
+            to="/profile"
+            className="mt-8 sm:mt-10 inline-block text-lg text-gray-300 font-medium"
+          >
+            <div className="flex items-center">
+              <span>Get in Touch</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 ml-1"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            </div>
+          </Link>
+        </div>
       </div>
       <div className="w-full sm:w-1/2 px-6 mt-12 sm:mt-0">
         <EditorStyled className="sm:pr-4">
