3c6cf18f016cd7652016af85aef7fdb8824e5e4e
muhrusdi <mhmrofficial@gmail.com>
2022-06-29 01:11:32 +0800

    setup css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     "./containers/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1.5rem"
+      },
+    },
   },
   plugins: [],
 }
