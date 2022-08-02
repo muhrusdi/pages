831261cf1e96177b1b02f6b54a4d5fdd188986d3
muhrusdi <mhmrofficial@gmail.com>
2022-08-02 13:33:30 +0800

    feat(utils): add date-fns, js-cookie

---
+++ utils/index.ts
@@ -0,0 +1,18 @@
+import cookie from "js-cookie"
+import { format } from "date-fns"
+
+export const getToken = () => cookie.get("accessToken")
+export const getCookie = (key: string) => cookie.get(key)
+export const setToken = (value: string) => cookie.set("accessToken", value)
+export const setCookie = (key: string, value: string) => cookie.set(key, value)
+export const removeCookie = (key: string) => cookie.remove(key)
+export const removeToken = () => cookie.remove("accessToken")
+export const isBrowser = typeof window !== "undefined"
+
+const dateIsValid = (date: string) => {
+  return !Number.isNaN(new Date(date).getTime())
+}
+
+export const formatDate = (val: string = new Date().toISOString(), str = "dd-MMM-yy") => {
+  return format(new Date(dateIsValid(val) ? val : new Date().toISOString()), str)
+}
