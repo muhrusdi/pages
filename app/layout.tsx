64676947ea455c679394506fdaf9fba283643d7c
muhrusdi <mhmrofficial@gmail.com>
2022-10-29 20:13:18 +0700

    chore: update css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import { RootLayoutType } from "@/types"
+import { Inter } from "@next/font/google"
+import clsx from "clsx"
 import "../styles/globals.css"
+
+const inter = Inter()
 
 const RootLayout: React.FC<RootLayoutType> = ({ children }) => {
   return (
-    <html lang="en">
+    <html lang="en" className={clsx(inter.className, "dark")}>
       <head>
-        <title>Next.js</title>
+        <title>Sen.js</title>
       </head>
-      <body>{children}</body>
+      <body className="dark:bg-slate-900 dark:text-white">{children}</body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./containers/**/*.{js,ts,jsx,tsx}",
   ],
