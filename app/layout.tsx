a85cf194843d722630efb3a1d577b1d157d9bb2f
muhrusdi <mhmrofficial@gmail.com>
2022-11-28 11:57:42 +0700

    chore: update meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,16 @@ import { Inter } from "@next/font/google"
 import clsx from "clsx"
 import "../styles/globals.css"
 
-const inter = Inter()
+const inter = Inter({
+  subsets: ["latin"],
+})
 
 const RootLayout: React.FC<RootLayoutType> = ({ children }) => {
   return (
     <html lang="en" className={clsx(inter.className, "dark")}>
       <head>
         <title>Sen.js</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="dark:bg-slate-900 dark:text-white">{children}</body>
     </html>
