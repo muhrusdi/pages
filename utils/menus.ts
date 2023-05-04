d14349fed423b40fa442708a54e1999b28a0100a
muhrusdi <mhmrofficial@gmail.com>
2023-05-04 23:53:45 +0800

    chore: add menus

---
+++ utils/menus.ts
@@ -0,0 +1,22 @@
+export const menus = [
+  {
+    id: "blog",
+    label: "Blog",
+    path: "/blog"
+  },
+  {
+    id: "work",
+    label: "Work",
+    path: "/work"
+  },
+  {
+    id: "story",
+    label: "Story",
+    path: "/story"
+  },
+  {
+    id: "profile",
+    label: "Profile",
+    path: "/profile"
+  },
+]
