813e26a8e8af7484f5bc5f1fe0f14c20fe74d5e3
muhrusdi <mhmrofficial@gmail.com>
2023-06-07 13:02:39 +0700

    feat: add blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,29 @@
+import { BlogItem } from "@/components/blog/blog-item"
+
 const Blog = () => {
-  return <div>blog</div>
+  return (
+    <div>
+      <div className="container">
+        <div className="grid grid-cols-3 gap-8">
+          <BlogItem
+            title="Lorem ipsum dolor sit amet consectetur, adipisicing elit"
+            description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate molestiae soluta eius"
+            image="https://plus.unsplash.com/premium_photo-1680721442350-6f91fbe7df3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2970&q=80"
+          />
+          <BlogItem
+            title="Lorem ipsum dolor sit amet consectetur, adipisicing elit"
+            description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate molestiae soluta eius"
+            image="https://plus.unsplash.com/premium_photo-1680721442350-6f91fbe7df3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2970&q=80"
+          />
+          <BlogItem
+            title="Lorem ipsum dolor sit amet consectetur, adipisicing elit"
+            description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate molestiae soluta eius"
+            image="https://plus.unsplash.com/premium_photo-1680721442350-6f91fbe7df3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2970&q=80"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Blog
