95b64cfc4366b6203e76a760069bc57154b4358e
muhrusdi <mhmrofficial@gmail.com>
2023-06-17 15:48:51 +0700

    add fakerjs

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "plus.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "loremflickr.com",
+      },
     ],
   },
 }
