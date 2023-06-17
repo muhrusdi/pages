32e5976f887be74c193c8770009a722e85d6edf4
muhrusdi <mhmrofficial@gmail.com>
2023-06-18 00:52:00 +0700

    update section

--- components/section/section.tsx
+++ components/section/section.tsx
@@ -3,26 +3,27 @@ type Props = {
   title: string
   description: string
   children: React.ReactNode
+  footer?: React.ReactNode
 }
 
 export const Section: React.FC<Props> = ({
   tag,
   title,
   description,
+  footer,
   children,
 }) => {
   return (
-    <div>
+    <div className="py-20">
       <div className="container">
         <div>
           <div className="text-center">
-            <span className="uppercase font-bold bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
-              {tag}
-            </span>
+            <span className="uppercase font-bold text-gradient">{tag}</span>
             <h2 className="text-3xl font-black mt-2">{title}</h2>
-            <p className="text-gray-400 mt-4">{description}</p>
+            <p className="text-gray-400 text-lg mt-4">{description}</p>
           </div>
-          <div>{children}</div>
+          <div className="mt-16">{children}</div>
+          {footer ? <div className="text-center mt-20">{footer}</div> : null}
         </div>
       </div>
     </div>
