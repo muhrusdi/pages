fbe785190d1f52266da30f947e5c0a693481cd03
Muhammad <mhmrofficial@gmail.com>
2024-01-31 15:07:43 +0700

    feat: add middleware

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+export function middleware(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers)
+
+  const accessToken = request.cookies.get("accessToken")
+
+  if (accessToken) {
+    requestHeaders.set("Authorization", "Bearer " + accessToken.value)
+  }
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
+
+  response.cookies.set("accessToken", process.env.ACCESS_TOKEN as string)
+
+  return response
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+}
