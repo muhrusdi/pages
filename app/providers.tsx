503a6ee614b41fd83b50e1663340b41e9e4543ce
Muhammad <mhmrofficial@gmail.com>
2024-01-31 15:07:32 +0700

    chore: update provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,26 @@
 "use client"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { revalidatePath, revalidateTag } from "next/cache"
+import { createContext, startTransition, useState } from "react"
+
 type Props = {
   children: React.ReactNode
 }
 
+type ProviderContextType = {
+  data: Record<string, any>
+  onChange: (val: string | number | Record<string, any>) => void
+} | null
+
+const ProviderContext = createContext<ProviderContextType>(null)
+
+const queryClient = new QueryClient()
+
 export const Providers: React.FC<Props> = ({ children }) => {
-  return <div>{children}</div>
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
 }
+
+export const StateContext = ProviderContext
