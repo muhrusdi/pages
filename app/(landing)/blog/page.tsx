a7981ed9d154781a42fc1f6752b4823549bde24c
muhrusdi <muhammadrusdiid@gmail.com>
2025-05-24 14:30:08 +0700

    feat: refactor blog component to read metadata from MDX files and improve data handling

--- app/(landing)/blog/page.tsx
+++ app/(landing)/blog/page.tsx
@@ -2,17 +2,50 @@ import { getData } from "@/lib/api"
 import { Header } from "@/components/utils"
 import { MetadataType } from "@/types"
 import { BlogItem } from "@/components/cards/blog"
+import path from "path"
+import fs from "fs"
 
-export const runtime = "edge"
+// export const runtime = "edge"
+export const dynamic = "force-static"
 
 const Blogs = async () => {
-  const data = await getData<{ blog: MetadataType[] }>("/contents")
+  // const data = await getData<{ blog: MetadataType[] }>("/contents")
+  const blogDirectory = path.join("app/(landing)/blog/(content)")
+  const postFilePaths = fs.readdirSync(blogDirectory)
+  const metadataRegex = /export\sconst\smetadata\s=\s{\s*([\s\S]*?)\s*}/
+
+  const blog = postFilePaths
+    .map(f => {
+      const fullPath = path.join(blogDirectory, f)
+      if (!fullPath.endsWith(".tsx")) {
+        const fullMDXPath = fullPath + "/page.mdx"
+        const fileName = f.replace(".mdx", "")
+        const file = fs.readFileSync(fullMDXPath, "utf-8")
+        const match = metadataRegex.exec(file)
+        const frontMatterBlock = match?.[1]
+        const frontmatterLines = frontMatterBlock?.trim().split("\n")
+        const metadata: Partial<MetadataType> = {}
+
+        frontmatterLines?.forEach(line => {
+          let [key, ...valueArr] = line.split(": ")
+          let value = valueArr.join(": ").trim()
+          value = value.replace(/^['",](.*)(['"],)$/, "$1") // Remove quotes
+          metadata[key.trim() as keyof MetadataType] = value
+        })
+
+        return {
+          ...metadata,
+          slug: "/blog/" + fileName,
+        }
+      }
+    })
+    .filter(Boolean) as MetadataType[]
 
   return (
     <div>
       <Header title="My Blogs" description="Here are some of my blogs." />
       <ul className="space-y-2">
-        {data?.blog?.map((item, i) => (
+        {blog?.map((item, i) => (
           <li key={i}>
             <BlogItem item={item} />
           </li>
