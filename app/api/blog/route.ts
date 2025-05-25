2e537e877e875f660b2dfdc26ed19a9e885e7a5d
muhrusdi <muhammadrusdiid@gmail.com>
2025-05-25 10:07:40 +0700

    refactor(blog): comment out unused blog directory and metadata processing code

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -5,38 +5,36 @@ import { MetadataType } from "@/types"
 export const dynamic = "force-static"
 
 export const GET = async () => {
-  const blogDirectory = path.join("app/(landing)/blog/contents")
-  const postFilePaths = fs.readdirSync(blogDirectory)
-  const metadataRegex = /export\sconst\smetadata\s=\s{\s*([\s\S]*?)\s*}/
+  // const blogDirectory = path.join("app/(landing)/blog/contents")
+  // const postFilePaths = fs.readdirSync(blogDirectory)
+  // const metadataRegex = /export\sconst\smetadata\s=\s{\s*([\s\S]*?)\s*}/
 
-  const blog = postFilePaths
-    .map(f => {
-      const fullPath = path.join(blogDirectory, f)
-      if (!fullPath.endsWith(".tsx")) {
-        const fullMDXPath = fullPath + "/page.mdx"
-        const fileName = f.replace(".mdx", "")
-        const file = fs.readFileSync(fullMDXPath, "utf-8")
-        const match = metadataRegex.exec(file)
-        const frontMatterBlock = match?.[1]
-        const frontmatterLines = frontMatterBlock?.trim().split("\n")
-        const metadata: Partial<MetadataType> = {}
+  // const blog = postFilePaths
+  //   .map(f => {
+  //     const fullPath = path.join(blogDirectory, f)
+  //     if (!fullPath.endsWith(".tsx")) {
+  //       const fullMDXPath = fullPath
+  //       const fileName = f.replace(".mdx", "")
+  //       const file = fs.readFileSync(fullMDXPath, "utf-8")
+  //       const match = metadataRegex.exec(file)
+  //       const frontMatterBlock = match?.[1]
+  //       const frontmatterLines = frontMatterBlock?.trim().split("\n")
+  //       const metadata: Partial<MetadataType> = {}
 
-        frontmatterLines?.forEach(line => {
-          let [key, ...valueArr] = line.split(": ")
-          let value = valueArr.join(": ").trim()
-          value = value.replace(/^['",](.*)(['"],)$/, "$1") // Remove quotes
-          metadata[key.trim() as keyof MetadataType] = value
-        })
+  //       frontmatterLines?.forEach(line => {
+  //         let [key, ...valueArr] = line.split(": ")
+  //         let value = valueArr.join(": ").trim()
+  //         value = value.replace(/^['",](.*)(['"],)$/, "$1") // Remove quotes
+  //         metadata[key.trim() as keyof MetadataType] = value
+  //       })
 
-        return {
-          ...metadata,
-          slug: "/blog/" + fileName,
-        }
-      }
-    })
-    .filter(Boolean)
+  //       return {
+  //         ...metadata,
+  //         slug: "/blog/" + fileName,
+  //       }
+  //     }
+  //   })
+  //   .filter(Boolean)
 
-  return Response.json({
-    blog,
-  })
+  return Response.json({})
 }
