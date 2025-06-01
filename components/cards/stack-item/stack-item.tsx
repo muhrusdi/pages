87c57c3f7771c70d39251f096e1b869c7c6543e6
muhrusdi <muhammadrusdiid@gmail.com>
2025-06-01 15:00:45 +0700

    feat: implement MagicCard component for enhanced card effects; update StackItem to utilize MagicCard

--- components/cards/stack-item/stack-item.tsx
+++ components/cards/stack-item/stack-item.tsx
@@ -1,13 +1,17 @@
+import { MagicCard } from "@/components/magicui/magic-card"
+
 type Props = {
   title?: string
 }
 
 const StackItem = ({ children, title }: React.PropsWithChildren<Props>) => {
   return (
-    <div className="flex min-h-full flex-col items-center rounded-md border border-gray-800 p-4">
-      <div>{children}</div>
-      <h4 className="mt-3 text-center text-sm">{title}</h4>
-    </div>
+    <MagicCard className="min-h-full rounded-md">
+      <div className="flex min-h-full flex-col items-center p-4">
+        <div>{children}</div>
+        <h4 className="mt-3 text-center text-sm">{title}</h4>
+      </div>
+    </MagicCard>
   )
 }
 
