ceea59b750cce219c6cbc398566ec546f35c25b4
muhrusdi <muhammadrusdiid@gmail.com>
2025-10-23 12:37:46 +0700

    feat: add proxy function to handle authorization and response cookies

--- _proxy.ts
+++ _proxy.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
-export const runtime = "edge"
-
 export function proxy(request: NextRequest) {
   const requestHeaders = new Headers(request.headers)
 
