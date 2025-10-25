4d4bbb70d9b4a5fee18ac03716408413bd4a2a84
muhrusdi <muhammadrusdiid@gmail.com>
2025-10-25 18:49:30 +0700

    fix: adjust SVG icon size for consistency

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -56,8 +56,8 @@ const Navigation = () => {
               onMouseLeave={() => handleHover(false)}
             >
               <svg
-                width="28"
-                height="28"
+                width="24"
+                height="24"
                 viewBox="0 0 745 745"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
